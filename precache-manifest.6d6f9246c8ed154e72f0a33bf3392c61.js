af8b690b9d325a59c1de44223fbed0b44e5ab5d4
Santosh <santoshkumarp1996@gmail.com>
2020-09-27 18:12:17 +0530

    Updates

---
+++ precache-manifest.6d6f9246c8ed154e72f0a33bf3392c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db5896e606672009f857175e8911cb9",
+    "url": "/SantoshK96/Connect-4/tree/master/connect4/index.html"
+  },
+  {
+    "revision": "bee79e6ee8f977dfb4b4",
+    "url": "/SantoshK96/Connect-4/tree/master/connect4/static/js/2.eaa53a4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SantoshK96/Connect-4/tree/master/connect4/static/js/2.eaa53a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c811beda4c7b2336041e",
+    "url": "/SantoshK96/Connect-4/tree/master/connect4/static/js/main.2983a68a.chunk.js"
+  },
+  {
+    "revision": "72d82007dbfcef45ef6b",
+    "url": "/SantoshK96/Connect-4/tree/master/connect4/static/js/runtime-main.a128c72c.js"
+  }
+]);
